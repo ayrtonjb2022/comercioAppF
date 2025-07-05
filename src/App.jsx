3bd404b9f2808326230c7b541cd2aedd15053433
Ayrton <benitezayrton.30@gmail.com>
2025-07-05 17:54:11 -0300

    sp

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,49 @@
 import './App.css'
-import RouterAP from "./router/Router";
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
+import PrivateRoute from './components/PrivateRouter'; // Asegúrate que el nombre del archivo esté bien escrito (mayúsculas/minúsculas)
+import Login from "./pages/LoginRegister";
+import Dashboard from "./pages/Dashboard";
 export default function App(){
 
-  return (
-    <>
-      <RouterAP/>
-    </>
-  );
-}
+    const [isAuthenticated, setIsAuthenticated] = useState(!!sessionStorage.getItem("token"));
+
+    useEffect(() => {
+        const checkAuth = () => {
+            setIsAuthenticated(!!sessionStorage.getItem("token"));
+        };
+
+        // Escucha cambios en otras pestañas (como cerrar sesión)
+        window.addEventListener("storage", checkAuth);
+
+        return () => {
+            window.removeEventListener("storage", checkAuth);
+        };
+    }, []);
+
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+
+                {/* Rutas Privadas */}
+                <Route
+                    path="/dashboard/*"
+                    element={
+                        <PrivateRoute isAuthenticated={isAuthenticated}>
+                            <Dashboard />
+                        </PrivateRoute>
+                    }
+                />
+
+                {/* Ruta por defecto si no se encuentra coincidencia */}
+                <Route
+                    path="*"
+                    element={<Navigate to={isAuthenticated ? "/dashboard" : "/"} replace />}
+                />
+            </Routes>
+        </Router>
+    );
+};
+
